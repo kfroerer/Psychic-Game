73366c70462c6d7c98ddc4df2b717b1f12f0d8a4
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-10-19 07:22:11 -0500

    still some work to do

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,50 @@
-var wins = 0;
-var losses = 0;
-var guess = 9;
-var wrongGuesses = "";
 var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", 
     "j", "k", "l", "m", "n", "o", "p", "q", "r",
     "s", "t", "u", "v", "x", "y", "z"];
-
-
-
-var userWins = document.getElementById('wins');
-var userLosses = document.getElementById('losses');
-var remainingGuesses = document.getElementById('left');
-var wrongGuesses = document.getElementById('lettersGuessed');
-
-
+var wins = 0;
+var losses = 0;
+var guess = 9;
 var randLetter = function (){
-   choices[MathFloor(Math.random() * (choices.length)-1)];
+    return choices[Math.floor(Math.random() * (choices.length -1))];
 }
+var ourGuess = randLetter();
+
+var node  = document.createElement("P");
 
 document.onkeyup = function (event) {
-    for (var i = 0; i <choices.length-1; i++){
-        var userGuess = event.key;
-    if (userGuess !== randLetter){
-        wrongGuesses.appendChild(userGuess);
-        userLosses++;
-        // not sure  
-        //document.write(userLosses); 
-        remainingGuesses--;
-        remainingGuesses.textContent(remainingGuesses);       
-    }else (userGuess === randLetter){
-        userWins++;
-       // not sure on this
-       // document.write(userWins);
+   
+    for (var i = 0; i < choices.length; i++){
+        var userGuess = event.key;  }  
+
+    if (userGuess !== ourGuess){
+        guess--;
+        document.getElementById("left").textContent = "Remaining Guesses: " + guess;
+        var wrongGuesses = document.createTextNode(userGuess)
+        node.appendChild(wrongGuesses);
+        document.getElementById("lettersGuessed").appendChild(node);
+    } //not working correctly
+    else if (guess <= 1) {
+        alert("You lose!");
+        losses++;
+        document.getElementById("losses").textContent = "Losses: " + losses;
+        ourGuess = randLetter();
+        guess = 9;
+        document.getElementById("left").textContent = "Remaining Guesses: " + guess;
+        node.parentNode.removeChild(wrongGuesses);
+
+        //not working....
+       // document.getElementById("lettesGuessed").textContent = "";
+
+    }else {
+        wins++;
+        document.getElementById("wins").textContent = "Wins: " + wins;
+        ourGuess = randLetter();
+        guess = 9;
+        document.getElementById("left").textContent = "Remaining Guesses: " + guess;
+        node.parentNode.removeChild(wrongGuesses);
+
+//not working
+       // document.getElementById("lettesGuessed").textContent = "Letters Guessed: ";
     }
-}
-};
     
-
-
-
-
+    }
